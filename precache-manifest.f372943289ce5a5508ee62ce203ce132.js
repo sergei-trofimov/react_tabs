9bb830e76e87410f1ebe312ae64ccb81fb3b5e1f
sergei-trofimov <feast.st@gmail.com>
2021-07-28 21:59:27 +0300

    Updates

---
+++ precache-manifest.f372943289ce5a5508ee62ce203ce132.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c366c80e22b6dbe67d4a164d8fdd824c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8f9ebc67f7932af096eb",
+    "url": "./static/css/main.eee85a7c.chunk.css"
+  },
+  {
+    "revision": "f4a6e332316f4a7ffe8f",
+    "url": "./static/js/2.50763e96.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "./static/js/2.50763e96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f9ebc67f7932af096eb",
+    "url": "./static/js/main.e783b1b0.chunk.js"
+  },
+  {
+    "revision": "53d1114eedfc0eaca354",
+    "url": "./static/js/runtime-main.a6df9e19.js"
+  }
+]);
